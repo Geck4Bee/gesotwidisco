fa92776cc3bfad96ce5742b145ae4c310e44e52e
geck4bee <geck4bee@gmail.com>
2021-01-22 11:58:18 +0000

    Basic Logic

--- index.py
+++ index.py
@@ -1,8 +1,39 @@
+import sys
+sys.path.append('lib')
+
+import requests
+import os
 import json
 import datetime
 
 
+def shaping_data(event):
+    return {
+        'ja': event['ja'],
+        'createdAt': event['createdAt'],
+        'url': event['url'],
+        'webhook': event['webhook']
+    }
+
+def sendWebHook(obj):
+    content = obj['ja'] + '\n' + obj['createdAt'] + '\n' + obj['url']
+    try:
+        print('shot webhook!')
+        response = requests.post(
+            obj['webhook'],
+            json.dumps({"content": content}),
+            headers={'Content-Type': 'application/json'}
+        )
+        print(response)
+    except Exception as ew:
+        sys.stderr.write("*** error *** in SendWebHook ***\n")
+        sys.stderr.write(str(ew) + "\n")
+    else:
+        return response
+
 def handler(event, context):
+    obj = shaping_data(event)
+    sendWebHook(obj)
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat()
